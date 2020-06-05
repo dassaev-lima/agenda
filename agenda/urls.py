a75a13edc2a2aa0d8e72055a42b685ae445f1036
dassaev-lima <dassaevnascimento@gmail.com>
2020-06-05 12:12:45 -0300

    incluído template para inserção de eventos da agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('',RedirectView.as_view(url='/agenda/')),
     path('login/',views.login_user),
     path('login/submit',views.submit_login),
-    path('logout',views.logout_user)
+    path('logout',views.logout_user),
+    path('agenda/evento/',views.evento),
+    path('agenda/evento/submit',views.evento_submit)
     #path('',views.index)
 ]
